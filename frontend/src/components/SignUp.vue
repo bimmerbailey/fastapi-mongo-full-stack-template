fc8d3efa14757acbae77e169da678875df3611c5
michael <steve_mikebailey@hotamil.com>
2023-05-07 14:23:43 -0400

    adding sign up page.

---
+++ frontend/src/components/SignUp.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { mdiAccount, mdiAsterisk } from '@mdi/js'
+import FormField from '@/components/FormField.vue'
+import FormControl from '@/components/FormControl.vue'
+
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+import BaseButtons from '@/components/BaseButtons.vue'
+import BaseButton from '@/components/BaseButton.vue'
+import type { AxiosError } from 'axios'
+
+const router = useRouter()
+const store = useAuthStore()
+
+const email: Ref<string | null> = ref(null)
+const password: Ref<string | null> = ref(null)
+const passwordCheck: Ref<string | null> = ref(null)
+const error: Ref<AxiosError | string | null> = ref(null)
+
+const emit = defineEmits(['signUpClose'])
+
+const close = () => {
+  error.value = null
+  email.value = null
+  password.value = null
+  passwordCheck.value = null
+  emit('signUpClose')
+}
+
+async function SignUp() {
+  if (email.value && password.value) {
+    if (password.value == passwordCheck.value) {
+      return await store
+        .createUser(email.value, password.value)
+        .then(async () => {
+          await router.push('/')
+        })
+        .catch((err) => {
+          return (error.value = err)
+        })
+    } else {
+      return (error.value = 'Passwords must match')
+    }
+  } else {
+    return (error.value = 'Email and password are required')
+  }
+}
+</script>
+
+<template>
+  <div>
+    <FormField label="Email" help="Please enter your email">
+      <FormControl
+        v-model="email"
+        :icon="mdiAccount"
+        name="email"
+        autocomplete="username"
+      />
+    </FormField>
+
+    <FormField label="Password" help="Please enter your password">
+      <FormControl
+        v-model="password"
+        :icon="mdiAsterisk"
+        type="password"
+        name="password"
+        autocomplete="current-password"
+      />
+    </FormField>
+
+    <FormField label="Confirm Password" help="Please re-enter your password">
+      <FormControl
+        v-model="passwordCheck"
+        :icon="mdiAsterisk"
+        type="password"
+        name="confirm-password"
+        autocomplete="current-password"
+      />
+    </FormField>
+    <div v-if="error" class="text-rose-700 text-center mb-5 font-bold">
+      {{ error }}
+    </div>
+    <BaseButtons class="justify-center">
+      <BaseButton label="Sign Up" @click="SignUp" />
+      <BaseButton label="Cancel" outline @click="close" />
+    </BaseButtons>
+  </div>
+</template>
+
+<style scoped></style>
--- frontend/src/views/ErrorView.vue
+++ frontend/src/views/ErrorView.vue
@@ -4,10 +4,8 @@
       <CardBox :class="cardClass">
         <div class="space-y-3">
           <h1 class="text-2xl">Unhandled exception</h1>
-
           <p>An Error Occurred</p>
         </div>
-
         <template #footer>
           <BaseButtons>
             <BaseButton label="Done" to="/" color="danger" />
