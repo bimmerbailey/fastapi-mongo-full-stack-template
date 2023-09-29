29a743551fcdc4c9447057e994acaa50dd4b3080
michael <steve_mikebailey@hotamil.com>
2023-09-29 07:33:27 -0400

    settings using proper config

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -1,25 +1,26 @@
 import os
 
+from pydantic import MongoDsn, ConfigDict
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_hostname: str = os.environ.get("DATABASE_HOST", "mongo")
-    database_port: str = os.environ.get("DATABASE_PORT", "27017")
-    database_password: str = os.environ.get("DATABASE_PASSWORD", "password")
-    database_name: str = os.environ.get("DATABASE_NAME", "your_app")
-    database_username: str = os.environ.get("DATABASE_USERNAME", "app_user")
-    secret_key: str = os.environ.get("JWT_SECRET_KEY", "secret")
-    algorithm: str = os.environ.get("JWT_ALGORITHM", "HS256")
-    access_token_expire_minutes: int = os.environ.get("JWT_TOKEN_EXPIRES", 60)
-    url_base: str = os.environ.get("URL_BASE", "localhost")
-    log_level: str = os.environ.get("LOG_LEVEL", "DEBUG")
-    json_logs: bool = os.environ.get("JSON_LOGS", False)
+    model_config = ConfigDict(frozen=True)
 
-    database_url: str = os.environ.get(
-        "DATABASE_URL",
-        f"mongodb://{database_username}:{database_password}@{database_hostname}/"
-        f"{database_name}?retryWrites=true&w=majority",
+    database_hostname: str = "mongo"
+    database_port: int = 27017
+    database_password: str = "password"
+    database_name: str = "your_app"
+    database_username: str = "app_user"
+    secret_key: str = "secret"
+    algorithm: str = "HS256"
+    jwt_token_expires: int = 60
+    url_base: str = "localhost"
+    log_level: str = "DEBUG"
+    json_logs: bool = False
+
+    database_url: MongoDsn = (
+        f"mongodb://{database_hostname}:{database_port}/{database_name}"
     )
 
 
