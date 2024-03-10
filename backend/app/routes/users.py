ed4c60fda4e6cff6b4db2c738fd262f1fabaa56f
michael <steve_mikebailey@hotamil.com>
2024-03-10 14:59:51 -0400

    formatting

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -2,13 +2,15 @@ from typing import Annotated
 
 import structlog
 from email_validator import EmailNotValidError, validate_email
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Security, Depends, HTTPException, status
 
 from app.dependencies.auth import CryptContext, get_crypt_context, get_current_user
 from app.models.users import User
 from app.schemas.users import UserCreate, UserOut
 
-router = APIRouter(prefix="/api/v1/users", tags=["Users"], dependencies=[Depends(get_current_user)])
+router = APIRouter(
+    prefix="/api/v1/users", tags=["Users"], dependencies=[Security(get_current_user)]
+)
 logger: structlog.stdlib.BoundLogger = structlog.getLogger(__name__)
 
 
