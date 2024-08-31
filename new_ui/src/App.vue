5a50dfbf219892abb5ed38d6c9c03995c35eda79
michael <steve_mikebailey@hotamil.com>
2024-08-31 10:54:45 -0400

    rewrite(frontend): setting default style in App.vue

--- new_ui/src/App.vue
+++ new_ui/src/App.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { ref } from 'vue'
+
+const currentYear = ref<number>(new Date().getFullYear())
 </script>
 
 <template>
-  <RouterView />
+  <div class="h-screen flex flex-col">
+    <header class="flex p-4 bg-gray-200">FAVM App</header>
+    <div class="flex flex-auto bg-gray-100 overflow-auto">
+      <main class="grow bg-blue-500 place-content-center gap-2">
+        <router-view />
+      </main>
+    </div>
+    <footer class="flex p-4 bg-gray-200">
+      <p class="text-center text-gray-500 text-xs">
+        Copyright &copy;{{ currentYear }} Your Business. All rights reserved.
+      </p>
+    </footer>
+  </div>
 </template>
 
 <style scoped></style>
