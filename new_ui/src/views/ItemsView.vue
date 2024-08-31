c1b8c611e0bd4b9c78e7b629d03ee6b52d5e13f1
michael <steve_mikebailey@hotamil.com>
2024-08-31 11:00:38 -0400

    rewrite(frontend): fixing type errors

--- new_ui/src/views/ItemsView.vue
+++ new_ui/src/views/ItemsView.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 import BaseCard from '@/components/bases/BaseCard.vue'
-import BaseTable from '@/components/bases/BaseTable.vue'
+import BaseTable, { type TableFieldsI } from '@/components/bases/BaseTable.vue'
 import { ref } from 'vue'
+import type { Item } from '@/interfaces/Items'
 
-const items = ref([])
-const itemHeaders = ref([])
+const items = ref<Item[]>([])
+const itemHeaders = ref<TableFieldsI[]>([{ text: 'Name', value: 'name' }])
 const itemCount = ref(0)
+const currentPage = ref(1)
+const perPage = ref(20)
 </script>
 
 <template>
   <base-card>
-    <base-table :items="items" :headers="itemHeaders" v-model="itemCount">
+    <base-table
+      :items="items"
+      :headers="itemHeaders"
+      pagination
+      :total-rows="itemCount"
+      v-model="currentPage"
+      :per-page="perPage"
+    >
     </base-table>
   </base-card>
 </template>
